0b56424a3dc13cde5ba91fc1734d714c9673b4e9
Mark Railton <marksrailton@gmail.com>
2023-08-29 09:42:54 +0100

    setup laravel backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('backup:clean')->dailyAt('03:15');
+        $schedule->command('backup:run')->dailyAt('03:30');
     }
 
     /**

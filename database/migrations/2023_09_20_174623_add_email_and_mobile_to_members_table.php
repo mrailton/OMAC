78d91c6070e165410eff5d67c25438ae5fa0efe8
Mark Railton <marksrailton@gmail.com>
2023-09-20 19:21:50 +0100

    Laravel Pint

--- database/migrations/2023_09_20_174623_add_email_and_mobile_to_members_table.php
+++ database/migrations/2023_09_20_174623_add_email_and_mobile_to_members_table.php
@@ -1,16 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
+return new class () extends Migration {
     public function up(): void
     {
-        Schema::table('members', function (Blueprint $table) {
+        Schema::table('members', function (Blueprint $table): void {
             $table->string('email')->nullable()->after('omac_id_number');
             $table->string('phone')->nullable()->after('email');
         });

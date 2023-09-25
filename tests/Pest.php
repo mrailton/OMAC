67488c4780d398f4464c2ac78e6d05813882425a
Mark Railton <mark.railton@outlook.ie>
2023-09-25 21:05:09 +0100

    Gotta run the migrations

--- tests/Pest.php
+++ tests/Pest.php
@@ -13,7 +13,10 @@ declare(strict_types=1);
 |
 */
 
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+
 uses(Tests\TestCase::class)->in('Feature');
+uses(LazilyRefreshDatabase::class)->in('Feature');
 
 /*
 |--------------------------------------------------------------------------

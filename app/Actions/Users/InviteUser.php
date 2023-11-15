0e9a3377c89b3b00115d176d516b99978902b006
Mark Railton <marksrailton@gmail.com>
2023-11-15 21:40:45 +0000

    cleaning up action for inviting users

---
+++ app/Actions/Users/InviteUser.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Actions\Users;
+
+use App\Mail\TeamInvitationMail;
+use App\Models\Invitation;
+use App\Models\User;
+use Filament\Notifications\Notification;
+use Illuminate\Support\Facades\Mail;
+
+class InviteUser
+{
+    public function execute(string $email): void
+    {
+        if (Invitation::query()->where('email', $email)->exists()) {
+            Notification::make('existingInvite')
+                ->body('This user has already been invited but has not accepted their invitation.')
+                ->danger()
+                ->send();
+
+            return;
+        }
+
+        if (User::query()->where('email', $email)->exists()) {
+            Notification::make('existingUser')
+                ->body('This email address is already being used by an existing user.')
+                ->danger()
+                ->send();
+
+            return;
+        }
+
+        $invitation = Invitation::create(['email' => $email]);
+
+        Mail::to($invitation->email)->send(new TeamInvitationMail($invitation));
+
+        Notification::make('invitedSuccess')
+            ->body('User invited successfully')
+            ->success()
+            ->send();
+    }
+}
--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Filament\Resources\UserResource\Pages;
 
+use App\Actions\Users\InviteUser;
 use App\Filament\Resources\UserResource;
 use App\Mail\TeamInvitationMail;
 use App\Models\Invitation;
+use App\Models\User;
 use Filament\Actions;
 use Filament\Forms\Components\TextInput;
 use Filament\Notifications\Notification;
@@ -27,15 +29,7 @@ class ListUsers extends ListRecords
                         ->required()
                 ])
                 ->action(function ($data) {
-                    $invitation = Invitation::create(['email' => $data['email']]);
-
-                    Mail::to($invitation->email)->send(new TeamInvitationMail($invitation));
-
-
-                    Notification::make('invitedSuccess')
-                        ->body('User invited successfully')
-                        ->success()
-                        ->send();
+                    (new InviteUser())->execute($data['email']);
                 }),
         ];
     }

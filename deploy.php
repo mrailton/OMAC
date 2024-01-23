1eee0db693965082f02aa8e38961c1bf6d6d534f
Mark Railton <marksrailton@gmail.com>
2024-01-23 21:51:55 +0000

    Configure deployer

---
+++ deploy.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+require 'contrib/php-fpm.php';
+
+set('application', 'omac');
+set('repository', 'git@github.com:mrailton/omac.git');
+set('php_fpm_version', '8.2');
+
+host(getenv('HOST'))
+    ->set('remote_user', getenv('USER'))
+    ->set('deploy_path', '~/www');
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'artisan:storage:link',
+    'artisan:view:cache',
+    'artisan:config:cache',
+    'artisan:migrate',
+    'deploy:publish',
+    'artisan:queue:restart',
+    'honeybadger:deploy',
+]);
+
+task('honeybadger:deploy', function () {
+    cd('{{release_path}}');
+    run('php artisan honeybadger:deploy');
+});
+
+after('deploy:failed', 'deploy:unlock');

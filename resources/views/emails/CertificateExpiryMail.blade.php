dadcd4958fdaae5ad09a3ba1f8b5185430a65fa4
Mark Railton <marksrailton@gmail.com>
2024-01-29 23:17:21 +0000

    Add active status to email

--- resources/views/emails/CertificateExpiryMail.blade.php
+++ resources/views/emails/CertificateExpiryMail.blade.php
@@ -3,10 +3,10 @@
 The following members have certificates that will expire in the next 3 months (or have already expired and not been updated). Please ensure this is corrected as soon as possible.
 
 <x-mail::table>
-| Name | CFR Cert Expiry | Clinical Cert Expiry | Manual Handling Cert Expiry | Garda Vetting Expiry |
-| ----- | -------- | -------- | -------- | -------- |
+| Name | Active | CFR Cert Expiry | Clinical Cert Expiry | Manual Handling Cert Expiry | Garda Vetting Expiry |
+| ----- | -------- | -------- | -------- | -------- | -------- |
 @foreach($members as $member)
-| {{ $member->name }} | @if($member->cfr_expires_on) {{ $member->cfr_expires_on->format('d/m/Y') }} @else No Date @endif | @if($member->cert_expires_on) {{ $member->cert_expires_on->format('d/m/Y') }} @else No Date @endif | @if($member->manual_handling_date) {{ $member->manual_handling_date->addYears(2)->format('d/m/Y') }} @else No Date @endif | @if($member->garda_vetting_date) {{ $member->garda_vetting_date->addYears(3)->format('d/m/Y') }} @else No Date @endif |
+| {{ $member->name }} | @if($member->active) Yes @else No @endif | @if($member->cfr_expires_on) {{ $member->cfr_expires_on->format('d/m/Y') }} @else No Date @endif | @if($member->cert_expires_on) {{ $member->cert_expires_on->format('d/m/Y') }} @else No Date @endif | @if($member->manual_handling_date) {{ $member->manual_handling_date->addYears(2)->format('d/m/Y') }} @else No Date @endif | @if($member->garda_vetting_date) {{ $member->garda_vetting_date->addYears(3)->format('d/m/Y') }} @else No Date @endif |
 @endforeach
 </x-mail::table>
 @else

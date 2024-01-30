129c9102b291dd1933ca591f649ac221931b3f98
Mark Railton <marksrailton@gmail.com>
2024-01-30 23:36:11 +0000

    Use healthcheck email for backup emails

--- config/backup.php
+++ config/backup.php
@@ -168,7 +168,7 @@ return [
         'notifiable' => Spatie\Backup\Notifications\Notifiable::class,
 
         'mail' => [
-            'to' => 'marksrailton@gmail.com',
+            'to' => env('HEALTH_CHECK_NOTIFICATIONS_EMAIL'),
 
             'from' => [
                 'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
